ce2394d8402c63e7b3e6c1584bd951201633a839
Quan Yang <quanyang@live.com>
2015-08-22 19:08:55 +0800

    Completed implementation for registration and login

--- src/HawkerHub/Controllers/UserController.php
+++ src/HawkerHub/Controllers/UserController.php
@@ -10,28 +10,32 @@ use \HawkerHub\Models\UserModel;
  * Controller class for all registration functions
  * @package HawkerHub
  **/
-class UserController extends \HawkerHub\Controllers\Controller{
+class UserController extends \HawkerHub\Controllers\Controller {
 
 	public function __construct($model) {
 		parent::__construct($model);
 	}
 
 	public function register($displayName, $provider, $providerUserId, $providerAccessToken) {
-		
-
-		
+		$app = \Slim\Slim::getInstance();
+		$success = UserModel::registerNewUser($displayName, $provider, $providerUserId, $providerAccessToken);
+		if (!$success) { 
+			$app->render(500, ['Status' => 'Registration failed.' ]);
+		} else {
+			$app->render(200, ['Status' => 'Registration successful.' ]);
+		}
 	}
 
 	public function login($providerUserId,$providerAccessToken) {
 		$app = \Slim\Slim::getInstance();
-		try {
-			$user = UserModel::loginByProviderUserIdAndAccessToken($providerUserId,$providerAccessToken);
+		$user = UserModel::loginByProviderUserIdAndAccessToken($providerUserId,$providerAccessToken);
+		if ($user) {
 			$app->render(200, ['user' => json_encode($user)]);
-		} catch(PDOException $e) {
-			$this->app->render(500, ['Status' => '{"error":{"text":'. $e->getMessage() .'}}']);
+		} else {
+			$app->render(500, ['Status' => 'Login failed.']);
 		}
 	}
 
 
 }
-?>
+
