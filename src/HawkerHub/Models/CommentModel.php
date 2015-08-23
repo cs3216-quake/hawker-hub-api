479d25d75b1d1daaad9300df838a1405eabb5bba
Quan Yang <quanyang@live.com>
2015-08-23 22:39:26 +0800

    Removed closing php tags

--- src/HawkerHub/Models/CommentModel.php
+++ src/HawkerHub/Models/CommentModel.php
@@ -7,36 +7,36 @@ require_once('DatabaseConnection.php');
 class CommentModel extends \HawkerHub\Models\Model{
 
   public $commentId;
-	public $commentDate;
-	public $userId;
-	public $postId;
+  public $commentDate;
+  public $userId;
+  public $postId;
   public $message;
 
   // Default constructor
-	public function __construct($commentId, $commentDate, $userId, $postId, $message) {
+  public function __construct($commentId, $commentDate, $userId, $postId, $message) {
     $this->commentId = $commentId;
-  	$this->commentDate = $commentDate;
-  	$this->userId = $userId;
-  	$this->postId = $postId;
+    $this->commentDate = $commentDate;
+    $this->userId = $userId;
+    $this->postId = $postId;
     $this->message = $message;
-	}
+  }
 
   public static function findCommentsByItem($itemId) {
     $result = [];
-		$db = \Db::getInstance();
+    $db = \Db::getInstance();
     $itemId = intval($itemId);
     $req = $db->prepare('SELECT * FROM Comment WHERE itemId = :itemId');
     // the query was prepared, now we replace :id with our actual $id value
-		$req->execute(array(
-			'itemId' => $itemId
-			));
+    $req->execute(array(
+     'itemId' => $itemId
+    ));    
 
       // we create a result of Like objects from the database results
-		foreach($req->fetchAll() as $comment) {
+    foreach($req->fetchAll() as $comment) {
       $result[] = new CommentModel($comment['commentId'], $comment['commentDate'], $comment['userId'], $comment['itemId'], $comment['message']);
-		}
+    }
 
-		return $result;
+    return $result;
   }
 
   public static function addCommentByItem($itemId, $userId, $message) {
@@ -45,14 +45,12 @@ class CommentModel extends \HawkerHub\Models\Model{
     $userId = intval($userId);
     $req = $db->prepare('INSERT INTO Comment (userId, itemId, message) VALUES (:userId, :itemId, :message)');
     // the query was prepared, now we replace :id with our actual $id value
-		$success = $req->execute(array(
+    $success = $req->execute(array(
       'userId' => $userId,
-			'itemId' => $itemId,
+      'itemId' => $itemId,
       'message' => $message
-			));
+      ));
 
-      return $success;
+    return $success;
   }
 }
-
-?>
